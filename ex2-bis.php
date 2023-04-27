1d214b4e181be5203b51b661b1e49cb4bb4fdaff
JackAdamsJenkins <clensx@gmail.com>
2023-04-27 14:48:40 +0200

    Correction EX2-bis

--- ex2-bis.php
+++ ex2-bis.php
@@ -1,7 +1,11 @@
 <?php 
+    /*
+        Ex2 + Choix de la TVA dans un select
+    */
     $message = null;
-    if(!empty($_GET['prixHT'])){
-        $message = "<span style=\"color:green\">Montant HT = ".$_GET['prixHT']."€, le prix TTC = ".$_GET['prixHT'] * 1.2 ."€</span>";
+    if(!empty($_GET['prixHT']) && !empty($_GET['TVA'])){
+        $tva = $_GET['TVA'] / 100 + 1;
+        $message = "<span style=\"color:green\">Montant HT = ".$_GET['prixHT']."€, le prix TTC = ".$_GET['prixHT'] * $tva ."€</span>";
     } else {
         $message = "<span style=\"color:red\">Veuillez entrer un montant</span>";
     }
@@ -18,6 +22,11 @@
     <h1>Calculer le prix TTC à partir du HT</h1>
     <form action="#" method="get">
         <input type="number" name="prixHT" placeholder="Entrez un prix HT">
+        <select name="TVA">
+            <option value="5.5">5.5%</option>
+            <option value="10">10%</option>
+            <option value="20">20%</option>
+        </select>
         <input type="submit" value="Calculer !">
     </form>
 
