c9fd49be00c2b6f94265183c34be913d5bb4904d
JackAdamsJenkins <clensx@gmail.com>
2023-05-02 15:23:09 +0200

    Correction exercice 12

--- ex12.php
+++ ex12.php
@@ -8,4 +8,32 @@ puis créez un script PHP qui calcule la somme de tous les entiers de 1 à ce no
 ex : 10
 Résultat : 55
 */
+$message = null;
+if(!empty($_GET['nombre'])){
+    $nombre = intval($_GET['nombre']);
+    $somme = 0;
+
+    for($i = 1; $i <= $nombre; $i++){
+        $somme += $i;
+    }
+    $message = "<p>La somme de $nombre est $somme</p>";
+}
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calcul de la somme des entiers</title>
+</head>
+<body>
+    <h1>Calcul de la somme des entiers</h1>
+    <form action="#" method="get">
+        <input type="number" name="nombre" placeholder="Nombre à calculer">
+        <input type="submit" value="Calculer">
+    </form>
+
+    <?= $message; ?>
+</body>
+</html>
