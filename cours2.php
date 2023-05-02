ec5659f88e949250656f110d8f723b7c45cff714
JackAdamsJenkins <clensx@gmail.com>
2023-05-02 11:54:35 +0200

    Cours foreach + exercice 9 et 9 bis

--- cours2.php
+++ cours2.php
@@ -93,6 +93,84 @@ while($i <= 100){ // Condition
     $i += 2; // Modification de l'amorce
 }
 
+/*
+    La boucle While à besoin :
+        - Avant son utilisation, d'initialiser un compteur ou une variable de condition (amorce)
+        - Dans le while(), on doit écrire la ou LES conditions qu'on souhaite voir remplies
+        - à l'intérieur de la condition { }, on doit écrire notre incrémentation OU modifier la variable de contion afin qu'elle arrive à terme (condition terminée)
 
+
+    $compteur = 0;
+    while(condition || autreCondition){
+        incrémentation, décrémentation, modification de la variable de condition
+    }
+*/
+
+/*
+    La boucle do...while s'exécute au moins une fois, quoi qu'il arrive
+
+    do {
+        // instructions
+    } while(condition);
+*/
+
+// Afficher les nombres pairs
+$i = 101;
+do {
+    // echo "Nombre pair $i <br/>";
+    $i += 2; 
+} while($i <= 100);
+
+/*
+    La boucle foreach (pour chaque) est essentielle pour parcourir un tableau
+    Grâce à cette boucle, il est possible d'obtenir soit la valeur, soit la clé (et la valeur également)
+
+    Tableau indexé :
+    0       1       2       3       (indice)
+    12      15      14      13      (valeur)
+
+    Tableau associatif
+    Jean    Paul    Jane    Julie   (clé)
+    12      15      14      13      (valeur)
+*/
+// $notes = array(12, 15, 14, 13);
+// var_dump($notes); // Voir toutes les données d'un tableau
+
+/*
+    foreach ren en premier paramètre un tableau
+    en deuxième paramètre, on nomme ce que contient le tableau
+
+    la tableau peut prendre un troisième paramètre
+    on peut lui dire de récupérer la clé et la valeur
+*/
+// foreach($notes as $note){
+//     echo "La note : $note <br/>";
+// }
+$notes = array(
+    "Jean" => 12,
+    "Paul" => 15,
+    "Jane" => 14,
+    "Julie" => 13
+);
+// var_dump($notes);
+// foreach($notes as $cle => $valeur){
+//     echo "La note de $cle est $valeur/20 <br/>";
+// }
+
+foreach($notes as $key => $value){
+    echo "La note de $key est $value/20 <br/>";
+}
+
+// Même chose que la ligne 160, mais vaut mieux éviter, c'est une mauvaise pratique, on ne sait pas à quoi correspondent $a et $b
+// foreach($notes as $a => $b){
+//     echo "La note de $a est $b/20 <br/>";
+// }
+
+/*
+    Ce qui est important dans le foreach c'est :
+        - L'ordre et la quantité de valeurs
+
+        (tableau as clé => valeur)
+*/
 
 ?>
---
+++ ex9-bis.php
@@ -0,0 +1,9 @@
+<?php 
+/*
+    Vous allez créer un tableau (associatif) en PHP
+    Dans un formulaire, vous allez demander de renseigner une valeur (number ou string)
+    Vous allez ajouter cette valeur à votre tableau en PHP
+    Ensuite, vous allez afficher sur la page toutes les données du tableau
+*/
+
+?>
