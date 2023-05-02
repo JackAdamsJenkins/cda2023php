e037eda9c108003e22670f1496e5a30e303b43d9
JackAdamsJenkins <clensx@gmail.com>
2023-05-02 14:46:19 +0200

    Correction exercice 10

--- ex10.php
+++ ex10.php
@@ -6,3 +6,28 @@ Utilisez la méthode GET pour récupérer la valeur,
 puis créez un script PHP qui affiche la table de multiplication pour ce nombre en utilisant une boucle for.
 */
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Table de multiplication</title>
+</head>
+<body>
+    <form action="#" method="get">
+        <input type="number" name="nombre" placeholder="Saisissez un nombre à multiplier">
+        <input type="submit" value="Multiplier !">
+    </form>
+    <?php 
+        if(!empty($_GET['nombre'])){
+            $nb = $_GET['nombre'];
+            // Faire la multiplication
+            for($i = 1; $i <= 10; $i++){
+                echo "$nb * $i = ". $nb * $i;
+                echo "<br/>";
+            }
+        }
+    ?>
+</body>
+</html>
--- ex11.php
+++ ex11.php
@@ -3,5 +3,22 @@
 Affichage de figures géométriques :
 Créez un script PHP qui utilise une boucle for pour afficher un triangle 
 composé de caractères spécifiques (par exemple, des astérisques).
+
+
+*
+**
+***
+****
+*****
+******
+*******
+********
+*********
+**********
+***********
+************
+
 */
+
+
 ?>
