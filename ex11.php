dcfbedf932299714a0883c0ab19628cf3bb0472e
JackAdamsJenkins <clensx@gmail.com>
2023-05-02 14:55:42 +0200

    Correction exercice 11

--- ex11.php
+++ ex11.php
@@ -17,8 +17,41 @@ composé de caractères spécifiques (par exemple, des astérisques).
 **********
 ***********
 ************
-
 */
-
-
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Triangle</title>
+</head>
+<body>
+    <h1>Afficher un triangle</h1>
+    <form action="#" method="get">
+        <input type="submit" name="show" value="Afficher">
+    </form>
+
+    <div>
+        <?php 
+            // Le formulaire a été envoyé
+            if(isset($_GET['show'])){
+                $triangleSize = 6;
+                // for($i = 1; $i <= $triangleSize; $i++){
+                //     for($j = 1; $j <= $i; $j++){
+                //         echo "*";
+                //     }
+                //     echo "<br/>";
+                // }
+
+                // Deuxième solution, avec une seul boucle
+                for($i = 1; $i <= $triangleSize; $i++) {
+                   echo str_repeat("*", $i) . "<br/>";
+                }
+            }
+
+        ?>
+    </div>
+</body>
+</html>
