54e64219b2dd0219ee7fa47b0b2ef29b9a840d31
JackAdamsJenkins <clensx@gmail.com>
2023-05-03 14:55:11 +0200

    Exercice 17

---
+++ ex17.php
@@ -0,0 +1,27 @@
+<?php 
+/*
+    Vous allez créer un système d'inscription/connexion
+
+    Vous allez avoir :
+        - Une page d'inscription, contenant :
+            - Un formulaire avec login, mot de passe
+            - Le mot de passe doit être hashé
+            - Les données doivent être stockées sur les cookies
+
+        - Une page de connexion, contenant :
+            - Un formulaire avec login, mot de passe
+            - Vérification du mot de passe stockée sur les cookies (hashé)
+            - Redirection sur la page profil si login/mdp ok
+
+        - Une page profil, contenant :
+            - Les données de login (mail ou pseudo)
+            - un lien vers la page "modification de profil"
+
+        - Une page modification de profil, contenant :
+            - La possibilité de modifier le login
+            - Possibilité de modifier/ajouter un prénom
+            - Possibilité de modifier/ajouter un nom
+            - Possibilité de modifier/ajouter une photo de profil
+
+*/
+?>
