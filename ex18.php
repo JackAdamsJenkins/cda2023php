23c365834a73912b1e87731f164238a42a0f5d72
JackAdamsJenkins <clensx@gmail.com>
2023-05-04 11:02:58 +0200

    Ajout exercice 18 - BDD

---
+++ ex18.php
@@ -0,0 +1,9 @@
+<?php
+/*
+    Vous allez faire un système de connexion/inscription
+
+    Vous allez stocker les données en base de données
+    Vous allez stocker le mot de passe de manière sécurisée
+    Vous allez enregistrer le pseudo de l'utilisateur sur les cookies si l'utilisateur est connecté
+*/
+?>
